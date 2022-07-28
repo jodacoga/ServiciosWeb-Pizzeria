5a9706dd570a3e06320101d8cc4dd4fbd8062039
José Quinde <49071271+JoseQ1996@users.noreply.github.com>
2022-07-27 19:10:48 -0500

    h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TipoPersona(idTipo,descripcion)VALUES(1,'Administrador');
+INSERT INTO TipoPersona(idTipo,descripcion)VALUES(2,'Empleado');
+INSERT INTO TipoPersona(idTipo,descripcion)VALUES(3,'Cliente');
+INSERT INTO Persona(cedula,apellido,fechaNacimiento,nombre,tipoUsuario_idTipo) VALUES ('0301681748','Quinde','1995-02-21','Jose',3);
