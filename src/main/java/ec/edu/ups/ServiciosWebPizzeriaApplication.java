9afe3ffafb8b24f07ccd3a7c6b3615a2093a1358
jodacoga <56452953+jodacoga@users.noreply.github.com>
2022-07-27 20:39:19 -0500

    Modificacion para Docker

--- src/main/java/ec/edu/ups/ServiciosWebPizzeriaApplication.java
+++ src/main/java/ec/edu/ups/ServiciosWebPizzeriaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 //@ComponentScan("ec.edu.ups")
 public class ServiciosWebPizzeriaApplication extends SpringBootServletInitializer {
 
